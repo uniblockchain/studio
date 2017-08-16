0f44d3bb7681a8ef543b86caa02bcd8aa474561a
Sam Rolfe <scrolfe@gmail.com>
2017-08-16 13:16:21 -0500

    working on adding big-calendar

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import BigCalendar from 'react-big-calendar';
-import globalize from 'globalize';
+import moment from 'moment';
+import events from '../events';
 
 BigCalendar.setLocalizer(
-  BigCalendar.globalizeLocalizer(globalize)
+  BigCalendar.momentLocalizer(moment)
 );
+
+let allViews = Object.keys(BigCalendar.views).map(k => BigCalendar.views[k])
 
 export default class Calendar extends React.Component {
 
   render(){
     return(
-      <BigCalendar />
+      <BigCalendar
+        {...this.props}
+        events={events}
+        views={allViews}
+        defaultDate={new Date(2015, 3, 1)}/>
     )
   }
 }
